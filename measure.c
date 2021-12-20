31f5e8b9965fdef311c77e328e844c600648bb93
hagai_hen <hagai349@gmail.com>
2021-12-20 18:12:36 +0200

    final

--- measure.c
+++ measure.c
@@ -1,93 +1,100 @@
-#include <sys/socket.h> 
+#include <sys/socket.h>
 #include <netinet/in.h>
-#include <netinet/tcp.h> 
-#include <stdio.h> 
-#include <string.h> 
-#include <unistd.h> 
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <time.h>
-#define SIZE 1024
-int fsize(FILE *fp){
-    int prev=ftell(fp);
+#define SIZE 1260785
+#define BILLION 1000000000.0
+
+int fsize(FILE *fp) {
+    int prev = ftell(fp);
     fseek(fp, 0L, SEEK_END);
-    int sz=ftell(fp);
-    fseek(fp,prev,SEEK_SET); //go back to where we were
+    int sz = ftell(fp);
+    fseek(fp, prev, SEEK_SET); //go back to where we were
     return sz;
 }
-void write_file(int sockfd){
-  int n;
-  double average = 0;
-  int num=0;
-  char buffer[SIZE];
-	FILE *fp;
-	char * filename = "1.txt";
-	fp = fopen(filename,"r");
-	int size = fsize(fp);
-	  clock_t t;
-  t = clock();
-  while (1) {
-    n = recv(sockfd, buffer, SIZE, 0);
-    if (n <= 0){
-      break;
-      return;
-      printf("the numbers of bytes is:%d",num);
+
+void write_file(int sockf) {
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+    int n = 0;
+    int num = 0;
+    char buffer[SIZE];
+    FILE *fp;
+    int tmp;
+    char *filename = "bigfile.txt";
+    fp = fopen(filename, "r");
+    int size = fsize(fp);
+    int count = 0;
+    int bool = 1;
+    int sum = 0;
+    while (sum <= 10*SIZE) {
+    	//printf("size: %d\n", size);
+        //tmp = n;
+        n = recv(sockf, buffer, SIZE, 0);
+        //printf("%d ", n);
+        if (n <= 0) {
+            break;
+            return;
+        }
+        sum = sum + strlen(buffer);
+        num = num + strlen(buffer);
+        if (sum >= 5*SIZE && bool) {
+            bool =0 ;
+            clock_gettime(CLOCK_REALTIME, &end);
+            double time_spent = (end.tv_sec - start.tv_sec) +
+                                (end.tv_nsec - start.tv_nsec) / BILLION;
+            printf("Time for transfer by cubic is: %f \n", time_spent);
+            clock_gettime(CLOCK_REALTIME, &start);
+            printf("The sum is: %d ", sum);
+        }
+        bzero(buffer, SIZE);
     }
-    num= num+strlen(buffer);
-	if(num>=size){
-		printf("the numbers of bytes is:%d \n",size);
-		num=num-size;
-		t = clock() - t;
-	double timeTaken = ((double)t)/CLOCKS_PER_SEC; // in seconds
-    printf("The time it took to transfer the file is: %f \n", timeTaken);
-	t = clock();
-    average += timeTaken;
-	}
-    bzero(buffer, SIZE);
-  }
-  printf("the average time for transfer file: %f \n",average);
-  printf("the numbers of bytes is:%d \n",num);
-  return;
+    clock_gettime(CLOCK_REALTIME, &end);
+    double time_spent = (end.tv_sec - start.tv_sec) +
+                        (end.tv_nsec - start.tv_nsec) / BILLION;
+    printf("Time for transfer by reno is: %f \n", time_spent);
+    //printf("%d", num);
+    printf("The sum is: %d\n", sum);
+    return;
 }
 
-int main(){
-  char *ip = "127.0.0.1";
-  int port = 10000;
+int main() {
+    char *ip = "127.0.0.1";
+    int port = 10000;
+    int sockf, new_sock;
+    struct sockaddr_in server_addr, new_addr;
+    socklen_t addr_size;
+    char buffer[SIZE];
 
-  int sockfd, new_sock;
-  struct sockaddr_in server_addr, new_addr;
-  socklen_t addr_size;
-  char buffer[SIZE];
+    sockf = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockf < 0) {
+        perror("socket error");
+        exit(1);
+    }
+    printf("Server socket created successfully.\n");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = port;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if(sockfd < 0) {
-    perror("Error in socket");
-    exit(1);
-  }
-  printf("Server socket created successfully.\n");
-
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-
-  int err = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if(err < 0) {
-    perror("Error in bind");
-    exit(1);
-  }
-  printf("Binding successfull\n");
-
-  if(listen(sockfd, 10) == 0){
-		printf("Connected....\n");
-	}else{
-		perror("Error in listening");
-    exit(1);
-	}
-
-  addr_size = sizeof(new_addr);
-  new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
-  write_file(new_sock);
-  printf("The file was received successfully");
-
-  return 0;
+    int err = bind(sockf, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (err < 0) {
+        perror("bind error");
+        exit(1);
+    }
+    printf("Binding successfully.\n");
+    if (listen(sockf, 10) == 0) {
+        printf("Connected...\n");
+    } else {
+        perror("Error in listening");
+        exit(1);
+    }
+    addr_size = sizeof(new_addr);
+    new_sock = accept(sockf, (struct sockaddr *) &new_addr, &addr_size);
+    write_file(new_sock);
+    printf("The file was received successfully");
+    return 0;
 }
+
--- sender.c
+++ sender.c
@@ -1,94 +1,95 @@
-#include <sys/socket.h> 
+#include <sys/socket.h>
 #include <netinet/in.h>
-#include <netinet/tcp.h> 
-#include <stdio.h> 
-#include <string.h> 
-#include <unistd.h> 
+#include <netinet/tcp.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
-#define SIZE 1024
-void send_file(FILE *fp, int sockf){
-  int n=0;
-  char data[SIZE] = {0};
-  while(fgets(data, SIZE, fp) != NULL) {
-    if (send(sockf, data,SIZE, 0) == -1) {
+#define SIZE 1260785
 
-      perror("Error in sending file.");
-      exit(1);
+void send_file(FILE *file, int sockf) {
+    int num = 0;
+    char data[SIZE] = {0};
+    while (fgets(data, SIZE, file) != NULL) {
+        if (send(sockf, data, SIZE, 0) == -1) {
+            perror("sending file error");
+            exit(1);
+        }
+        num = num + strlen(data);
+        bzero(data, SIZE);
     }
-	n=n+strlen(data);
-    bzero(data, SIZE);
-  }
-  printf("%d bytes were sent \n",n);
+    printf("%d bytes were sent", num);
 }
 
 int main(int argc, char **argv) {
-char buf[256];
-socklen_t len;
-int sock = socket(AF_INET, SOCK_STREAM, 0);
- 	if (sock == -1) {
- 		perror("socket");
- return -1;
- } 
+    char buf[256];
+    socklen_t len;
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("socket");
+        return -1;
+    }
 
-len = sizeof(buf); 
-if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) { 
-perror("getsockopt");
-return -1;
-} 
+    len = sizeof(buf);
+    if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) {
+        perror("getsockopt");
+        return -1;
+    }
 
-printf("Current: %s\n", buf); 
-// my code
-  char *ip = "127.0.0.1";
-  int port = 10000;
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-  
-    int err = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-      if(err == -1) {
-    perror("Error in socket");
-    exit(1);
-  }
-  printf("Connected to Server.\n");
-  char * filename = "1.txt";
-for(int i=0;i<5;i++){
-    FILE * fp;
-    fp = fopen(filename, "r");
-  if (fp == NULL) {
-    perror("Error in reading file.");
-    exit(1);
-  }
-    send_file(fp, sock);
-    fclose(fp);
-  printf("File data sent successfully.\n");
+    printf("Current: %s\n", buf);
+    char *ip = "127.0.0.1";
+    int port = 10000;
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = port;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+
+    int err = connect(sock, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (err == -1) {
+        perror("socket error");
+        exit(1);
+    }
+    printf("Connected to Server.\n");
+    char *filename = "bigfile.txt";
+    for (int i = 0; i < 5; i++) {
+        FILE *file;
+        file = fopen(filename, "r");
+        if (file == NULL) {
+            perror("Error in reading file.");
+            exit(1);
+        }
+        send_file(file, sock);
+        fclose(file);
+        printf("File data sent successfully.\n");
+    }
+
+    strcpy(buf, "reno");
+    len = strlen(buf);
+    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0) {
+        perror("setsockopt");
+        return -1;
+    }
+    len = sizeof(buf);
+    if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) {
+        perror("getsockopt");
+        return -1;
+    }
+    printf("New: %s\n", buf);
+    for (int i = 0; i < 5; i++) {
+        FILE *file;
+        file = fopen(filename, "r");
+        if (file == NULL) {
+            perror("reding file error");
+            exit(1);
+        }
+        send_file(file, sock);
+        fclose(file);
+        printf("File data sent successfully.\n");
+    }
+    printf("closing connection");
+
+    close(sock);
+    return 0;
 }
 
-strcpy(buf, "reno"); 
-len = strlen(buf);
-if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0) {
-perror("setsockopt"); 
-return -1;
-}
-len = sizeof(buf); 
-if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) {
-perror("getsockopt"); 
-return -1; 
-} 
-printf("New: %s\n", buf);
-for(int i=0;i<5;i++){
-    FILE * fp;
-    fp = fopen(filename, "r");
-  if (fp == NULL) {
-    perror("Error in reading file.");
-    exit(1);
-  }
-    send_file(fp, sock);
-    fclose(fp);
-  printf("File data sent successfully.\n");
-}
-printf("closing connection");
-close(sock); 
-return 0; 
-}
